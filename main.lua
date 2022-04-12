67a2927b5688ac6b00aec829630dd357f8ce144c
byyako <46730266+byyako@users.noreply.github.com>
2022-04-12 18:55:29 -0500

    Initial upload

---
+++ main.lua
@@ -0,0 +1,6 @@
+local LSM = LibStub("LibSharedMedia-3.0")
+
+--- DEFAULT INSTALLED ---
+LSM:Register([[sound]], [[AMM: Discord]], [[Interface\AddOns\MediaManager\Sounds\discord_sounds.mp3]])
+
+--- BEGIN APP SOUNDS ---
